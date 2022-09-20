0dcb46098a27b8862e09e95bb5e06544c68e5a15
Danah111 <113886108+Danah111@users.noreply.github.com>
2022-09-20 13:06:22 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import draw from "./img2.svg";
+import Nav from "./Nav";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hi my name is dana and I love clothes and owning bags, I love business management, I specialize in management information systems, and one of my hobbies is learning computer languages ​​to reach and inquire on my private number 0503484222
+<img src={draw} className='img2' ></img>
+      
+  < Nav/>   
+    </div>
+  );
+}
+
+export default App;
---
+++ Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Nav() {
+  return (
+
+    <div>
+ <ul className='list'>
+ <li>danah</li>
+ <li>saad</li>
+
+ </ul>
+ </div> )
+}export default Nav
